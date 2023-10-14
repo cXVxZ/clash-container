4018ad106c514399f2140e256895168dbd3a1857
cXVxZ <102743782+cXVxZ@users.noreply.github.com>
2023-10-14 21:02:44 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,26 +4,34 @@
 ip rule add fwmark 666 lookup 666
 ip route add local 0.0.0.0/0 dev lo table 666
 
-# CREATE TABLE
-iptables-nft -t mangle -N clash
+# `clash` chain for using tproxy to redirect traffic to the clash listening port 7893
+iptables -t mangle -N clash
 
-# RETURN LOCAL AND LANS
-iptables-nft -t mangle -A clash -d 0.0.0.0/8 -j RETURN
-iptables-nft -t mangle -A clash -d 10.0.0.0/8 -j RETURN
-iptables-nft -t mangle -A clash -d 127.0.0.0/8 -j RETURN
-iptables-nft -t mangle -A clash -d 169.254.0.0/16 -j RETURN
-iptables-nft -t mangle -A clash -d 172.16.0.0/12 -j RETURN
-iptables-nft -t mangle -A clash -d 192.168.50.0/16 -j RETURN
-iptables-nft -t mangle -A clash -d 192.168.9.0/16 -j RETURN
+# skip traffic to LAN or reserved address
+# reference:
+# https://en.wikipedia.org/wiki/List_of_assigned_/8_IPv4_address_blocks
+# https://en.wikipedia.org/wiki/Private_network#IPv4
+# IANA - Local Identification
+iptables -t mangle -A clash -d 0.0.0.0/8 -j RETURN
+# IANA - Loopback
+iptables -t mangle -A clash -d 127.0.0.0/8 -j RETURN
+# IANA - Private Use
+iptables -t mangle -A clash -d 10.0.0.0/8 -j RETURN
+iptables -t mangle -A clash -d 172.16.0.0/12 -j RETURN
+iptables -t mangle -A clash -d 192.168.0.0/16 -j RETURN
+# IPv4 Link-Local Addresses
+iptables -t mangle -A clash -d 169.254.0.0/16 -j RETURN
+# Multicast
+iptables -t mangle -A clash -d 224.0.0.0/4 -j RETURN
+# Future Use
+iptables -t mangle -A clash -d 240.0.0.0/4 -j RETURN
 
-iptables-nft -t mangle -A clash -d 224.0.0.0/4 -j RETURN
-iptables-nft -t mangle -A clash -d 240.0.0.0/4 -j RETURN
+# Forward all the other traffic to port 7893 and set tproxy mark
+iptables -t mangle -A clash -p tcp -j TPROXY --on-port 7893 --tproxy-mark 666
+iptables -t mangle -A clash -p udp -j TPROXY --on-port 7893 --tproxy-mark 666
 
-# FORWARD ALL
-iptables-nft -t mangle -A clash -p udp -j TPROXY --on-port 7893 --tproxy-mark 666
-iptables-nft -t mangle -A clash -p tcp -j TPROXY --on-port 7893 --tproxy-mark 666
+# Append the `clash` chain to PREROUTING to enable it
+iptables -t mangle -A PREROUTING -j clash
 
-# REDIRECT
-iptables-nft -t mangle -A PREROUTING -j clash
 
 /clash
